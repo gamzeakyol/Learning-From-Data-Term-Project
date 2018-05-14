66b615d4f931da260efa1bafb1cfd2f9f4d6ffd9
Mahmut Lutfullah Özbilen <mahmutlozbilen@gmail.com>
2018-05-14 14:15:55 +0300

    preprocess files for test and train are split

--- datapreprocess.py
+++ datapreprocess.py
@@ -1,43 +1,36 @@
 # -*- coding: utf-8 -*-
 """
 Created on Mon May  7 17:23:36 2018
-
 @author: mloz & gam
 """
 
 import numpy as np
-import matplotlib.pyplot as plt
 import pandas as pd
 from scipy import stats
 
 
-dataset = pd.read_csv('train.txt')
-'''returns = dataset['returnShipment']
-del dataset['returnShipment']'''
-dataset['age'] = -1
-dataset['deliveryDuration'] = -1
-#dataset['returnShipment'] = returns
-dataset = np.array(dataset, dtype = str)
 
-hayda = 0
+trainDataset = pd.read_csv('train.txt')
+trainDataset['age'] = -1
+trainDataset['deliveryDuration'] = -1
+
+trainDataset = np.array(trainDataset, dtype = str)
+
 year = []
 month = []
 day = []
 difference = []
-for i in range(len(dataset)):
-        dataset[i][4] = dataset[i][4].lower()
-        '''if dataset[i][9] == "Mr":
-                dataset[i][9] = "1"
-        else: 
-                dataset[i][9] = "0"'''
+for i in range(len(trainDataset)):
+        trainDataset[i][4] = trainDataset[i][4].lower()
+
                         
-        if dataset[i][2] != '?' and dataset[i][1] != '?' and dataset[i][10] != '?': 
-                year.append((int(dataset[i][2].split("-")[0]) - int(dataset[i][1].split("-")[0]))*365)
-                month.append((int(dataset[i][2].split("-")[1]) - int(dataset[i][1].split("-")[1]))*30)
-                day.append((int(dataset[i][2].split("-")[2]) - int(dataset[i][1].split("-")[2])))
+        if trainDataset[i][2] != '?' and trainDataset[i][1] != '?' and trainDataset[i][10] != '?': 
+                year.append((int(trainDataset[i][2].split("-")[0]) - int(trainDataset[i][1].split("-")[0]))*365)
+                month.append((int(trainDataset[i][2].split("-")[1]) - int(trainDataset[i][1].split("-")[1]))*30)
+                day.append((int(trainDataset[i][2].split("-")[2]) - int(trainDataset[i][1].split("-")[2])))
                 difference.append(year[-1] + month[-1] + day[-1])
-                dataset[i][14] = int(dataset[i][1].split("-")[0]) - int(dataset[i][10].split("-")[0])
-                dataset[i][15] = difference[i]
+                trainDataset[i][14] = int(trainDataset[i][1].split("-")[0]) - int(trainDataset[i][10].split("-")[0])
+                trainDataset[i][15] = difference[i]
                 
                 if difference[i] < 0:
                         difference[i] = "Time error"
@@ -46,13 +39,13 @@ for i in range(len(dataset)):
                 month.append('error')
                 day.append('error')
                 difference.append('error')
-                dataset[i][14] = 'error'
-                dataset[i][15] = 'error'
+                trainDataset[i][14] = 'error'
+                trainDataset[i][15] = 'error'
 
-items = np.array(dataset[:,3],int)
-colors = np.array(dataset[:,5],str)
+items = np.array(trainDataset[:,3],int)
+colors = np.array(trainDataset[:,5],str)
 
-item_color = np.zeros((2943, 84),int)
+#item_color = np.zeros((2945, 84),int)
 
 unique_colors = set(colors)
 uniqueColors = []
@@ -61,19 +54,18 @@ for i in unique_colors:
 
 uniqueColors = np.array(uniqueColors)
 
-for i in range(len(dataset)):
+'''for i in range(len(dataset)):
         color = dataset[i][5]
         for j in range(84):
                 if uniqueColors[j] == color:
                         break
 
-        item_color[int(dataset[i][3])-1,j] += 1
-hayda = 0
-for i in range(len(dataset)):
+"      item_color[int(dataset[i][3])-1,j] += 1'''
+'''for i in range(len(dataset)):
         if dataset[i][5] == '?':
                 dataset[i][5] = uniqueColors[np.argmax(item_color[int(dataset[i][3])])]
-
-sizes = np.array(dataset[:,4],str)
+'''
+sizes = np.array(trainDataset[:,4],str)
 unique_sizes = set(sizes)
 uniqueSizes = []
 for i in unique_sizes:
@@ -81,7 +73,7 @@ for i in unique_sizes:
 
 uniqueSizes = np.array(uniqueSizes)
      
-genders = np.array(dataset[:,9],str)
+genders = np.array(trainDataset[:,9],str)
 unique_genders = set(genders)
 uniqueGenders = []
 for i in unique_genders:
@@ -89,7 +81,7 @@ for i in unique_genders:
 
 uniqueGenders = np.array(uniqueGenders)
 
-states = np.array(dataset[:,11],str)
+states = np.array(trainDataset[:,11],str)
 unique_states = set(states)
 uniqueStates = []
 for i in unique_states:
@@ -97,29 +89,29 @@ for i in unique_states:
 
 uniqueStates = np.array(uniqueStates)
 
-for i in range(len(dataset)):
+for i in range(len(trainDataset)):
         for j in range(len(uniqueGenders)):
-                if dataset[i][9] == uniqueGenders[j]:
-                        dataset[i][9] = j
+                if trainDataset[i][9] == uniqueGenders[j]:
+                        trainDataset[i][9] = j
                         break
         for j in range(len(uniqueSizes)):
-                if dataset[i][4] == uniqueSizes[j]:
-                        dataset[i][4] = j
+                if trainDataset[i][4] == uniqueSizes[j]:
+                        trainDataset[i][4] = j
                         break
         for j in range(len(uniqueColors)):
-                if dataset[i][5] == uniqueColors[j]:
-                        dataset[i][5] = j
+                if trainDataset[i][5] == uniqueColors[j]:
+                        trainDataset[i][5] = j
                         break
         for j in range(len(uniqueStates)):
-                if dataset[i][11] == uniqueStates[j]:
-                        dataset[i][11] = j
+                if trainDataset[i][11] == uniqueStates[j]:
+                        trainDataset[i][11] = j
                         break
 
 noErrorDataset = []
 
-for i in range(len(dataset)):
-        if dataset[i][2] != '?' and dataset[i][5] != '?' and dataset[i][10] != '?' and difference[i] != 'Time error':
-                noErrorDataset.append(dataset[i])
+for i in range(len(trainDataset)):
+        if trainDataset[i][2] != '?' and trainDataset[i][5] != '?' and trainDataset[i][10] != '?' and difference[i] != 'Time error':
+                noErrorDataset.append(trainDataset[i])
         
 noErrorDataset = np.array(noErrorDataset)
 noErrorDataset = np.delete(noErrorDataset, 12, 1)
@@ -129,17 +121,10 @@ noErrorDataset = np.delete(noErrorDataset, 1, 1)
 noErrorDataset = np.delete(noErrorDataset, 0, 1)
 noErrorDataset = np.array(noErrorDataset,float) 
 
-#dataset = pd.read_csv('gamah.csv')
-#dataset = np.array(dataset)
-'''temp = noErrorDataset[:,8]
-noErrorDataset[:,8] = noErrorDataset[:,10]
-noErrorDataset[:,10] = temp
-'''
 noErrorDataset[:,[8, 10]] = noErrorDataset[:,[10, 8]]
 
 
 noErrorDataset[:, :10] = stats.zscore(noErrorDataset[:, :10],axis=0)
 
 
-np.savetxt("gamah.csv", noErrorDataset, delimiter=",", fmt="%s")
-
+np.savetxt("trainDataset.csv", noErrorDataset, delimiter=",", fmt="%s")
